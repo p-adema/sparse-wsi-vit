c7dd945ff92c5bb8e1d5cda612c8a645eff2e8e8
p-adema <peter.adema@student.uva.nl>
2026-04-22 21:25:58 +0200

    Updated preprocessing scripts

---
+++ scripts/upload_snellius.py
@@ -0,0 +1,31 @@
+# File upload utility -- can run in parallel to preprocessing script, uploading new files as they are created.
+
+import time
+from pathlib import Path
+from subprocess import run
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--local", type=Path, required=True)
+parser.add_argument("--remote", type=Path, required=True)
+parser.add_argument("--host", type=str, default="snellius-dl2")
+args = parser.parse_args()
+
+# Check paths exist
+assert args.local.exists(), f"No such path {args.local}"
+run(["ssh", args.host, "ls", args.remote]).check_returncode()
+
+processed = set()
+
+print("Starting file search")
+while True:
+    new_files = [f for f in args.local.glob("*.h5") if f not in processed]
+    if new_files:
+        print(f"found {len(new_files)} files...")
+    # Wait for files to be saved properly
+    time.sleep(60)
+    for file in new_files:
+        run(["scp", file, f"{args.host}:{args.remote / file.name}"]).check_returncode()
+    processed.update(new_files)
+    if new_files:
+        print("\tuploaded.")
--- scripts/virchow_preprocess.py
+++ scripts/virchow_preprocess.py
@@ -15,11 +15,12 @@ import pathlib
 import tqdm
 import h5py
 import argparse
-import threading
+from concurrent.futures import ThreadPoolExecutor
+from typing import NamedTuple
 
 print("Loading model...")
 with torch.inference_mode(), torch.autocast("cuda", torch.bfloat16):
-    model = (
+    VIRCHOW2 = (
         timm.create_model(
             "hf-hub:paige-ai/Virchow2",
             pretrained=True,
@@ -30,77 +31,201 @@ with torch.inference_mode(), torch.autocast("cuda", torch.bfloat16):
         .cuda()
     )
 
-    transforms = create_transform(
-        **resolve_data_config(model.pretrained_cfg, model=model)
+    TRANSFORMS = create_transform(
+        **resolve_data_config(VIRCHOW2.pretrained_cfg, model=VIRCHOW2)
+    )
+    EXECUTOR = ThreadPoolExecutor()
+
+
+def find_roi(
+        slide: OpenSlide, mask_path: Path
+) -> tuple[npt.NDArray, npt.NDArray, npt.NDArray, npt.NDArray]:
+    """
+    We want to find regions of interest which are sized 112x112 when downsampled 16x, such
+    that we have 8x8=64 tokens per region at patch size 224x224, so we can apply NSA even
+    at the most zoomed-out level.
+
+    We also compute an `int112`, which is a mask for which subblocks of size 56x56 are
+    relevant within this patch, and an `int56`, which is the same for subblocks of 28x28.
+
+    Using these subblocks, we can do NSA at a higher resolution, e.g. using 28x28 masks
+    (which are downsampled 16x from the original image, so represent a 448x448 patch)
+    and patch size 56x56 we still have 8x8=64 tokens in the block.
+
+    Returns:
+        roi_x (n_roi)
+        roi_y (n_roi)
+        int112 (n_roi, 2, 2)
+        int56 (n_roi, 4, 4)
+    """
+    down16 = slide.level_dimensions[0][0] // 16, slide.level_dimensions[0][1] // 16
+    mask = np.asarray(Image.open(mask_path).resize(down16)) == 0
+    mask_windows = np.lib.stride_tricks.sliding_window_view(mask, (112, 112))[
+        ::112, ::112
+    ]
+    roi_y, roi_x = np.nonzero(mask_windows.sum(axis=(2, 3)) > 112 ** 2 * 0.1)
+    n_regs = len(roi_y)
+    subwindows56 = mask_windows[roi_y, roi_x].reshape(n_regs, 2, 56, 2, 56)
+    int112 = subwindows56.sum((2, 4)) > 56 ** 2 * 0.1
+    subwindows28 = mask_windows[roi_y, roi_x].reshape(n_regs, 4, 28, 4, 28)
+    int56 = subwindows28.sum((2, 4)) > 28 ** 2 * 0.1
+    return roi_x, roi_y, int112, int56
+
+
+def estimate_size_gb(slide_path: pathlib.Path, mask_path: pathlib.Path) -> float:
+    slide = openslide.OpenSlide(slide_path)
+    roi, _, _, _ = find_roi(slide, mask_path)
+    # For each roi, we have (at most) 64 56x56 patch embeddings, 16 112x112 patch
+    # embeddings, 1 224x224 patch embedding and 1 224x224 CLS token, all 1280dim F16.
+    return len(roi) * (64 + 16 + 2) * 16 * 1280 * 2 / 1e9
+
+
+def load_region(slide: OpenSlide, x: int, y: int) -> torch.Tensor:
+    region = slide.read_region((x * 8 * 224, y * 8 * 224), 0, (8 * 224, 8 * 224))
+    region = (
+        torch.from_numpy(np.asarray(region).astype(np.float32) / 255.0)[..., :3]
+        .view(8, 224, 8, 224, 3)
+        .permute(4, 0, 2, 1, 3)
+        .reshape(64, 3, 224, 224)
+    )
+    return TRANSFORMS(region)
+
+
+class Result(NamedTuple):
+    coords_56x56: npt.NDArray
+    coords_112x112: npt.NDArray
+    coords_224x224: npt.NDArray
+    patches_56x56: npt.NDArray
+    patches_112x112: npt.NDArray
+    patches_224x224: npt.NDArray
+    cls_224x224: npt.NDArray
+
+
+def process_slide(slide_path: pathlib.Path, mask_path: pathlib.Path) -> Result:
+    slide = OpenSlide(slide_path)
+    roi_x, roi_y, int112, int56 = find_roi(slide, mask_path)
+
+    c_glob = torch.asarray(np.stack((roi_y, roi_x), axis=-1) * 8 * 224)  # (roi, 2)
+    c_tile = torch.arange(0, 8 * 4) * 4 * 14
+    c_loc = torch.cartesian_prod(c_tile, c_tile)  # (32x32, 2)
+
+    coords_56x56 = (
+        (c_glob[:, None] + c_loc[None, :])
+        .view(len(roi_x), 4, 8, 4, 8, 2)
+        .transpose(2, 3)
+        .reshape(len(roi_x), 4, 4, 64, 2)
+    )
+    coords_112x112 = coords_56x56[:, ::2, ::2]
+    coords_224x224 = coords_56x56[:, 0, 0]
+
+    patches_56x56 = np.empty((len(roi_x), 4, 4, 64, 1280), dtype=np.float16)
+    patches_112x112 = np.empty((len(roi_x), 2, 2, 64, 1280), dtype=np.float16)
+    patches_224x224 = np.empty((len(roi_x), 64, 1280), dtype=np.float16)
+    cls_224x224 = np.empty((len(roi_x), 64, 1280), dtype=np.float16)
+
+    load_handle = EXECUTOR.submit(load_region, slide, roi_x[0], roi_y[0])
+    for i in tqdm.trange(0, len(roi_x), desc="processing", leave=False):
+        region = load_handle.result()
+        if i + 1 < len(roi_x):
+            load_handle = EXECUTOR.submit(
+                load_region, slide, roi_x[i + 1], roi_y[i + 1]
+            )
+
+        cls, emb_224x224, emb_112x112, emb_56x56 = process_region(region.cuda())
+
+        cls_224x224[i] = cls.cpu().numpy()
+        patches_224x224[i] = emb_224x224.cpu().numpy()
+        patches_112x112[i] = emb_112x112.cpu().numpy()
+        patches_56x56[i] = emb_56x56.cpu().numpy()
+
+    return Result(
+        coords_56x56=coords_56x56.numpy()[int56],
+        coords_112x112=coords_112x112.numpy()[int112],
+        coords_224x224=coords_224x224.numpy(),
+        patches_56x56=patches_56x56[int56],
+        patches_112x112=patches_112x112[int112],
+        patches_224x224=patches_224x224,
+        cls_224x224=cls_224x224,
     )
 
 
-def find_roi(slide: OpenSlide, mask_path: Path) -> tuple[npt.NDArray, npt.NDArray]:
-    down16 = slide.level_dimensions[0][0] // 16, slide.level_dimensions[0][1] // 16
-    mask = np.asarray(Image.open(mask_path).resize(down16)) == 0
-    mask_windows = np.lib.stride_tricks.sliding_window_view(mask, (14, 14))[::14, ::14]
-    roi_y, roi_x = np.nonzero(mask_windows.sum(axis=(2, 3)) > 50)
-    return roi_x, roi_y
+@torch.compile(mode="max-autotune")
+def process_region(region):
+    output = VIRCHOW2(region).view(64, 261, 1280)
+
+    # first tokens are CLS then 4 register tokens, then 16x16=256 embedding tokens.
+    cls = output[:, 0]
+
+    emb_56x56: torch.Tensor = (
+        output[:, 5:]
+        .view(8, 8, 4, 4, 4, 4, 1280)  # [BY, BX, TY, Pool Y, TX, Pool X, Dim]
+        .mean((3, 5))
+        .transpose(1, 2)
+        .reshape(4, 8, 4, 8, 1280)
+        .transpose(1, 2)
+        .reshape(4, 4, 64, 1280)
+    )
+
+    emb_112x112: torch.Tensor = (
+        output[:, 5:]
+        .view(8, 8, 2, 8, 2, 8, 1280)  # [BY, BX, TY, Pool Y, TX, Pool X, Dim]
+        .mean((3, 5))
+        .transpose(1, 2)
+        .reshape(2, 8, 2, 8, 1280)
+        .transpose(1, 2)
+        .reshape(2, 2, 64, 1280)
+    )
+    emb_224x224: torch.Tensor = output[:, 5:].view(64, 256, 1280).mean(1)
+
+    return cls, emb_224x224, emb_112x112, emb_56x56
 
 
-def estimate_size(slide_path: pathlib.Path, mask_path: pathlib.Path) -> float:
-    slide = openslide.OpenSlide(slide_path)
-    roi, _ = find_roi(slide, mask_path)
-    return len(roi) * 16 * 1280 * 2 / 1e9
-
-
-def process_slide(
-        slide_path: pathlib.Path, mask_path: pathlib.Path
-) -> tuple[npt.NDArray, npt.NDArray]:
-    slide = openslide.OpenSlide(slide_path)
-    roi_x, roi_y = find_roi(slide, mask_path)
-
-    regions = torch.empty((len(roi_x), 3, 224, 224), dtype=torch.float16)
-    coords_glob = torch.asarray(np.stack((roi_x, roi_y), axis=-1) * 16)
-    coords_loc = torch.cartesian_prod(torch.arange(0, 4), torch.arange(0, 4))
-    coords = (coords_glob[:, None] + coords_loc[None, :]).view(len(roi_x) * 16, 2) * 14
-    for i, (x, y) in enumerate(zip(roi_x, roi_y, strict=True)):
-        reg = slide.read_region((x * 224, y * 224), 0, (224, 224)).convert("RGB")
-        regions[i] = transforms(reg)
-
-    patches = np.empty((len(roi_x), 16, 1280), dtype=np.float16)
-    batch_size = 256
-    with torch.inference_mode(), torch.autocast("cuda", torch.float16):
-        for i in tqdm.trange(
-                0, len(roi_x), batch_size, desc="processing", disable=True
-        ):
-            output = model(regions[i: i + batch_size].cuda())  # size: B x 261 x 1280
-
-            # size: B x 256 x 1280, tokens 1-4 are register tokens so we ignore those
-            patch = output[:, 5:].view(-1, 4, 4, 4, 4, 1280).mean((2, 4))
-            patches[i: i + batch_size] = patch.cpu().view(-1, 16, 1280).numpy()
-
-    return patches.reshape(coords.shape[0], 1280), coords.numpy()
-
-
-def save_to_disk(out_path: pathlib.Path, features: npt.NDArray, coords: npt.NDArray):
+def save_to_disk(out_path: Path, res: Result):
     try:
         with h5py.File(out_path, mode="w") as file:
-            file["features"] = features
-            file["coords"] = coords
+            file["coords_56x56"] = res.coords_56x56
+            file["coords_112x112"] = res.coords_112x112
+            file["coords_224x224"] = res.coords_224x224
+            file["patches_56x56"] = res.patches_56x56
+            file["patches_112x112"] = res.patches_112x112
+            file["patches_224x224"] = res.patches_224x224
+            file["cls_224x224"] = res.cls_224x224
     except KeyboardInterrupt:
         out_path.unlink(missing_ok=True)
+        raise
 
     verify_output(out_path)
 
 
 def verify_output(out_path: Path):
     assert out_path.stat().st_size > 0, f"Zero-size {out_path=}"
+    keys = {
+        "coords_56x56",
+        "coords_112x112",
+        "coords_224x224",
+        "patches_56x56",
+        "patches_112x112",
+        "patches_224x224",
+        "cls_224x224",
+    }
     with h5py.File(out_path) as file:
-        assert len(file["coords"].shape) == 2, f"Strange file at {out_path=}"
-        assert len(file["features"].shape) == 2, f"Strange file at {out_path=}"
-        coords_n, coords_2 = file["coords"].shape
-        features_n, features_d = file["features"].shape
-        assert coords_2 == 2, f"Strange file at {out_path=}"
-        assert features_d == 1280, f"Strange file at {out_path=}"
-        assert coords_n == features_n, f"Strange file at {out_path=}"
+        file_keys = set(file.keys())
+        assert file_keys == keys, f"Incorrect {file_keys=} for {out_path=}"
+        for key in keys:
+            shp = file[key].shape[1:]
+            assert len(shp) == 2
+            if "coords" in key:
+                assert shp == (64, 2)
+            else:
+                assert shp == (64, 1280)
+
+        for size in ("56x56", "112x112", "224x224"):
+            assert file[f"coords_{size}"].shape[0] == file[f"patches_{size}"].shape[0]
+        assert file["coords_224x224"].shape[0] == file["cls_224x224"].shape[0]
 
 
+@torch.inference_mode
+@torch.autocast("cuda", torch.bfloat16)
 def main(slides: pathlib.Path, masks: pathlib.Path, output: pathlib.Path, check: bool):
     assert slides.exists() and slides.is_dir(), f"Invalid {slides=}"
     assert masks.exists() and masks.is_dir(), f"Invalid {masks=}"
@@ -118,7 +243,11 @@ def main(slides: pathlib.Path, masks: pathlib.Path, output: pathlib.Path, check:
     for slide_path, mask_path in paths:
         out_path = (output / slide_path.name).with_suffix(".h5")
         if out_path.exists():
-            verify_output(out_path)
+            try:
+                verify_output(out_path)
+            except AssertionError as e:
+                e.add_note(f"Failed at {out_path}")
+                raise
             processed_outputs.append(out_path)
         else:
             unprocessed_paths.append((slide_path, mask_path))
@@ -130,28 +259,24 @@ def main(slides: pathlib.Path, masks: pathlib.Path, output: pathlib.Path, check:
     if check:
         total_size = 0
         for slide_path, mask_path in tqdm.tqdm(paths, desc="verifying slides"):
-            total_size += estimate_size(slide_path, mask_path)
+            total_size += estimate_size_gb(slide_path, mask_path)
 
         print("All files seem OK.")
         print(f"Total estimated output: {total_size:.1f} GB")
         return
 
-    save_handle = threading.Thread()
-    save_handle.start()
     for slide_path, mask_path in tqdm.tqdm(unprocessed_paths, desc="processing slides"):
         try:
-            features, coords = process_slide(slide_path, mask_path)
+            res = process_slide(slide_path, mask_path)
         except (Exception, KeyboardInterrupt) as e:
-            e.add_note(f"Failed on {slide_path = }")
+            e.add_note(f"Failed on {slide_path=}")
             raise
 
         out_path = (output / slide_path.name).with_suffix(".h5")
         # The hard drive I'm using for this is slow, so I put this on a separate thread.
-        save_handle.join()
-        save_handle = threading.Thread(
-            target=save_to_disk, args=(out_path, features, coords)
-        )
-        save_handle.start()
+        EXECUTOR.submit(save_to_disk, out_path, res)
+
+    EXECUTOR.shutdown()
 
 
 if __name__ == "__main__":
