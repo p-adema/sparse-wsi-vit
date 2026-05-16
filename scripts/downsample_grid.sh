bbd15fb999d1210ffbd5d1f68a531562fe4ff72e
p-adema <peter.adema@student.uva.nl>
2026-05-16 20:32:31 +0200

    Fix TCGA TMB config

--- scripts/downsample_grid.sh
+++ scripts/downsample_grid.sh
@@ -1,35 +1,47 @@
 #!/usr/bin/env bash
 set -e
-# todo: scheduler patience
+
+nq /home/peter/reset-ram
+
+sleep 0.1
+nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_tcga_tmb.py \
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False \
+  net.checkpoint_activations=False scheduler.patience=10 dataset.features_name=cls_224x224
+
+sleep 0.1
+nq /home/peter/reset-ram
+
+sleep 0.1
 nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
   dataset.downscale_block=2 dataset.features_name=cls_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
 
 sleep 0.1
 nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
   dataset.downscale_block=4 dataset.features_name=cls_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
-
-sleep 0.1
-nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
-  dataset.downscale_block=2 dataset.features_name=patches_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
-
-nq sh -c 'echo "Downsample grid halfway, reset RAM" | mail -s "Reset RAM" peter.adema@student.uva.nl'
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
 
 sleep 0.1
 nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
   dataset.downscale_block=8 dataset.features_name=cls_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
+
+sleep 0.1
+nq /home/peter/reset-ram
+
+sleep 0.1
+nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
+  dataset.downscale_block=2 dataset.features_name=patches_224x224 dataset.label_col_name=is_tumor \
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
 
 sleep 0.1
 nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
   dataset.downscale_block=4 dataset.features_name=patches_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
 
 sleep 0.1
 nq uv run experiments/run.py --config src/sparse_wsi_vit/configs/baseline_vit5dense_preembed_camelyon.py \
   dataset.downscale_block=8 dataset.features_name=patches_224x224 dataset.label_col_name=is_tumor \
-  train.iterations=8000 scheduler.total_iterations=8000 dataset.pin_memory=False
+  train.iterations=5000 scheduler.total_iterations=5000 dataset.pin_memory=False scheduler.patience=30
 
 nq sh -c 'echo "Downsample grid complete" | mail -s "Jobs done" peter.adema@student.uva.nl'
